1199554caffd46e2fe8a68cdd665545648e8b345
Tom <123583284+taaooum@users.noreply.github.com>
2024-12-07 00:59:55 +0100

    added old projects

---
+++ binary-calculator.cs
@@ -0,0 +1,55 @@
+﻿using System.Dynamic;
+
+namespace _20._10._2023_Binnärrechner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Schreibe eine bliebige Natürliche Zahl die in Binär Code umgewanelt werden soll:");
+                string input =null;
+                do
+                {
+                    input = Console.ReadLine();
+                    if (input == "" )
+                    {
+                        Console.WriteLine("Versuche es Nocheinmal:");
+                    }
+                    
+                }while (input == "");
+
+                string zahlBinär = "";
+                try
+                {
+                    int dezimalZahl;
+                    dezimalZahl = Convert.ToInt32(input);
+                    zahlBinär = Codierer(dezimalZahl);
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+                
+                Console.WriteLine($"Die Zahl {input} schreibt man in Binär Code: {zahlBinär}");
+                Console.ReadLine();
+            }while (true);
+
+        }
+        static string Codierer(int input)
+        {
+            string antwort = null;
+            int rest = 0;
+            do
+            {
+                input = input / 2;
+                rest = input % 2;
+                antwort = antwort + rest ;
+            } while (input != 0);
+
+            return antwort;
+        }
+    }
+}
---
+++ exponent-calculator.cs
@@ -0,0 +1,62 @@
+﻿namespace Exponent_Methode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1;
+            string num2;
+            
+            double basis = 0;
+            double exponent = 0;
+            
+            do 
+            {
+                Console.Write("Gebe eine Basis ein: ");
+                num1 = Console.ReadLine();
+                Console.Write("Gebe eine Exponent ein: ");
+                num2 = Console.ReadLine();
+
+                try
+                {
+                    basis = Convert.ToDouble(num1);
+                    exponent = Convert.ToDouble(num2);
+                }catch (Exception ex)
+                {
+                    Console.WriteLine("Gebe eine Zahl ein und keine Zeichen.");
+                }
+
+                double result = GetExponent(basis, exponent);
+                if (result != -1)
+                {
+                    Console.WriteLine("Die Antwort ist "+result +"\n");
+                }
+            }while (num1 != null || num2 == null);
+        }
+
+        static double GetExponent(double basis, double exponent)
+        {
+            if (exponent== 1)
+            {
+                return basis;
+            }else if (exponent == 0)
+            {
+                return 0;
+            }
+            else if (basis != 0)
+            {
+                double result = 0;
+                for (double i = 0; i <= exponent; i++) 
+                { 
+                    result = basis * basis;
+                }
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Bitte gebe Zahlen an mit den ich rechnen kann. 0 als Basis ergibt immer 0...\n");
+                return -1;
+            }
+        }
+    }
+}
